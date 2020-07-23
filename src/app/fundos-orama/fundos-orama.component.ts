1db95d2f2f23167752e98fb58c18a0169ea763fc
Phelipe Chiarelli GMF <pchiarelli@gmfsaneamento.com.br>
2020-07-23 17:56:34 -0300

    Filtro por tipo de investimento
    Filtro por tipo de investimento já está funcional

--- src/app/fundos-orama/fundos-orama.component.ts
+++ src/app/fundos-orama/fundos-orama.component.ts
@@ -13,11 +13,12 @@ export class FundosOramaComponent implements OnInit {
 
   listaFundos: ListaFundosModel[];
   paramPesquisa: string = '';
-  paramFiltro: number;
+  paramFiltro: number = 0;
   err: any;
   values: string = '';
   listaFundosConst: ListaFundosModel[];
   listaTipoFundos: FundMainStrategyModel[] = [];
+  listaIds: number[] = [];
 
   screen: boolean;
 
@@ -26,22 +27,19 @@ export class FundosOramaComponent implements OnInit {
     this.checkScreenSize();
     this.getListaFundos();
 
-   }
+  }
 
   ngOnInit() {
 
   }
 
-  checkScreenSize(){
+  checkScreenSize() {
     if (window.screen.availWidth < 850) {
       this.screen = true; // dimensão de tela acima de 850px
     } else {
       this.screen = false; // dimensão de tela abaixo de 850px
-
     }
-    console.log('checkScreenSize:' + this.screen);
   }
-
 
   onResize(event) {
     event.target.innerWidth;
@@ -50,63 +48,75 @@ export class FundosOramaComponent implements OnInit {
     } else {
       this.screen = false; // dimensão de tela abaixo de 850px
     }
-    console.log('onResize: ' +this.screen);
   }
 
-
-  getListaFundos(){
+  getListaFundos() {
     this.fundosService.getListaFundos().subscribe(
       (dadosFundos) => {
         this.listaFundosConst = dadosFundos;
-        this.listaFundos = this.listaFundosConst;
-        console.log(this.listaFundos);
+        // this.listaFundos = this.listaFundosConst;
+        this.listaFundos = this.listaFundosConst.filter(data => { return data.specification.fund_macro_strategy.id === 1 });
 
-        this.listaTipoFundos = Array.from(this.listaFundosConst.filter(data =>
-          { return data.specification.fund_macro_strategy.id === 1 }).reduce((m, t) => m.set(t.specification.fund_main_strategy.name, t.specification.fund_main_strategy), new Map()).values());
-        console.log(this.listaTipoFundos);
-        console.log(this.paramPesquisa);
-        console.log(this.paramFiltro);
+        this.listaTipoFundos = Array.from(this.listaFundosConst.filter(data => { return data.specification.fund_macro_strategy.id === 1 }).reduce((m, t) => m.set(t.specification.fund_main_strategy.name, t.specification.fund_main_strategy), new Map()).values());
       },
       (error: any) => {
         this.err = error;
-        console.log(this.err);
+
       }
     )
   }
 
+  filtroFundos(dataFundos) {
+    let listaFundosAUX: ListaFundosModel[];
+    this.listaFundos = [];
+    if (dataFundos.checked == true) {
+      this.listaIds.push(dataFundos.value);
+    } else {
+      this.listaIds = this.listaIds.filter(obj => obj !== dataFundos.value);
+    }
+    this.listaIds.map(data => {
+      listaFundosAUX = this.listaFundosConst.filter(d => { return d.specification.fund_main_strategy.id === data });
+
+      listaFundosAUX.map(item => {
+        this.listaFundos.push(item)
+      })
+    })
+
+  }
+
   mapperFundosRisco(valor: number) {
     switch (valor) {
-      case 1 : {return 'fundos risco-1'};
-      case 2 : {return 'fundos risco-2'};
-      case 3 : {return 'fundos risco-3'};
-      case 4 : {return 'fundos risco-4'};
-      case 5 : {return 'fundos risco-5'};
-      case 6 : {return 'fundos risco-6'};
-      case 7 : {return 'fundos risco-7'};
-      case 8 : {return 'fundos risco-8'};
-      case 9 : {return 'fundos risco-9'};
-      case 10 : {return 'fundos risco-10'};
-      case 11 : {return 'fundos risco-11'};
-      case 12 : {return 'fundos risco-12'};
-      default : {return 'fundos'};
+      case 1: { return 'fundos risco-1' };
+      case 2: { return 'fundos risco-2' };
+      case 3: { return 'fundos risco-3' };
+      case 4: { return 'fundos risco-4' };
+      case 5: { return 'fundos risco-5' };
+      case 6: { return 'fundos risco-6' };
+      case 7: { return 'fundos risco-7' };
+      case 8: { return 'fundos risco-8' };
+      case 9: { return 'fundos risco-9' };
+      case 10: { return 'fundos risco-10' };
+      case 11: { return 'fundos risco-11' };
+      case 12: { return 'fundos risco-12' };
+      default: { return 'fundos' };
     }
   }
 
   mapperFundosRiscoMobile(valor: number) {
     switch (valor) {
-      case 1 : {return 'card risco-mobile-1'};
-      case 2 : {return 'card risco-mobile-2'};
-      case 3 : {return 'card risco-mobile-3'};
-      case 4 : {return 'card risco-mobile-4'};
-      case 5 : {return 'card risco-mobile-5'};
-      case 6 : {return 'card risco-mobile-6'};
-      case 7 : {return 'card risco-mobile-7'};
-      case 8 : {return 'card risco-mobile-8'};
-      case 9 : {return 'card risco-mobile-9'};
-      case 10 : {return 'card risco-mobile-10'};
-      case 11 : {return 'card risco-mobile-11'};
-      case 12 : {return 'card risco-mobile-12'};
-      default : {return 'card'};
+      case 1: { return 'card risco-mobile-1' };
+      case 2: { return 'card risco-mobile-2' };
+      case 3: { return 'card risco-mobile-3' };
+      case 4: { return 'card risco-mobile-4' };
+      case 5: { return 'card risco-mobile-5' };
+      case 6: { return 'card risco-mobile-6' };
+      case 7: { return 'card risco-mobile-7' };
+      case 8: { return 'card risco-mobile-8' };
+      case 9: { return 'card risco-mobile-9' };
+      case 10: { return 'card risco-mobile-10' };
+      case 11: { return 'card risco-mobile-11' };
+      case 12: { return 'card risco-mobile-12' };
+      default: { return 'card' };
     }
   }
 }
